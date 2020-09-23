08e29bdeb0f0e16804bb462722fc36e6ff8e245d
L-ik <2394398957@qq.com>
2020-09-23 22:33:34 +0800

    第一次向GitHub提交代码

---
+++ day01.py
@@ -0,0 +1,34 @@
+"""
+print("hello world!")  #字符串
+print("你好！")
+print(2333)
+print(2.333)
+print(True)      
+print(())
+print({})
+print([])
+
+print("sdjj","哈哈哈")
+print("哈哈哈"+"黑恶hi")
+print("和"*10)
+print((4+8-3*2)/3)
+print(2>6)
+
+#变量
+#赋值
+name="妖姬"
+print(name)
+"""
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+
+# a="就覅偶是金佛i交给开口看过"
+# print(len(a))
+#练习：通过代码获取两段内容，并且计算他们的长度的和。
+
+a = (input("请输入："))
+b = (input("请输入："))
+# print("获取的第一段内容：",a)
+# print("获取的第一段内容：",b)
+print("长度和：",len(a)+len(b))
